b0ded2f28ff42f907dc446dbe4df15024ac7d8a6
初旭 <chuxu@shulie.io>
2021-09-01 16:57:33 +0800

    config

--- config/config.ts
+++ config/config.ts
@@ -48,7 +48,7 @@ export default {
           prefetch: true,
           apps: [{
             name: 'pro', // 子应用唯一id
-            entry: 'http://192.168.1.71/tro-pro', // html entry
+            entry: 'http://192.168.1.71/tro-pro/index.html', // html entry
             // entry: 'http://192.168.63.15:8001/',
             base: '/pro', // 子应用的路由前缀，通过这个前缀判断是否要启动该应用，通常跟子应用的 base 保持一致
             mountElementId: 'app-slave', // 子应用挂载点
