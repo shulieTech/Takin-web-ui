36b2320465da3e13e3e928638342c65b8c03e75e
谢舒羽 <xieshuyu@xieshuyudeMacBook-Pro.local>
2021-09-06 11:02:07 +0800

    样式

--- src/layouts/components/ContentNode.tsx
+++ src/layouts/components/ContentNode.tsx
@@ -24,13 +24,18 @@ const ContentNode: React.FC = props => {
         minHeight: `100%`,
         height: '100%'
       }}
+      className="flex flex-1 of-x-hd of-y-at"
     >
-      <div
-        id="app-slave"
+      <Content
+        className={
+          venomBasicConfig.layout === 'header' &&
+            venomBasicConfig.contentWidthMode === 'fixed'
+            ? styles.wrap
+            : 'flex'
+        }
         style={{
           flexDirection: 'column',
           // padding: '16px',
-          display: 'block',
           backgroundColor: '#fff',
           marginLeft: '8px',
           marginRight: '8px',
@@ -38,40 +43,9 @@ const ContentNode: React.FC = props => {
           borderRadius: '4px 4px 0 0',
           overflow: 'scroll'
         }}
-      />
-      <div
-        style={{
-          // paddingTop: venomBasicConfig.fixHeader
-          //   ? venomBasicConfig.headerHeight
-          //   : 0,
-          background: venomBasicConfig.contentBg,
-          // minHeight: `calc(100% - ${footerHeight}px)`
-          minHeight: `100%`,
-          height: '100%'
-        }}
-        className="flex flex-1 of-x-hd of-y-at"
       >
-        <Content
-          className={
-            venomBasicConfig.layout === 'header' &&
-              venomBasicConfig.contentWidthMode === 'fixed'
-              ? styles.wrap
-              : 'flex'
-          }
-          style={{
-            flexDirection: 'column',
-            // padding: '16px',
-            backgroundColor: '#fff',
-            marginLeft: '-8px',
-            marginRight: '8px',
-            marginTop: '8px',
-            borderRadius: '4px 4px 0 0',
-            overflow: 'scroll'
-          }}
-        >
-          {props.children}
-        </Content>
-      </div>
+        {props.children}
+      </Content>
     </div>
   );
 };
--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -5,7 +5,20 @@ import router from 'umi/router';
 export default class NotFound extends Component {
   render() {
     return (
-      <div style={{ display: 'none' }} />
+      <div
+        id="app-slave"
+        style={{
+          flexDirection: 'column',
+          // padding: '16px',
+          width: '100%',
+          backgroundColor: '#fff',
+          marginLeft: '8px',
+          marginRight: '8px',
+          marginTop: '8px',
+          borderRadius: '4px 4px 0 0',
+          overflow: 'scroll'
+        }}
+      />
     );
   }
 }
