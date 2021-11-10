4a81ae164a358548c86b7e6c21b06f56648fc4a3
谢舒羽 <xieshuyu@xieshuyudeMacBook-Pro.local>
2021-11-10 16:10:50 +0800

    fix:默认

--- src/pages/user/loginPage.tsx
+++ src/pages/user/loginPage.tsx
@@ -108,9 +108,10 @@ export default class Login extends DvaComponent<Props, State> {
     if (key) {
       localStorage.setItem('tenant-code', key.replace(/\//, ''));
       localStorage.setItem('env-code', 'test');
+    } else {
+      localStorage.setItem('tenant-code', 'default');
+      localStorage.setItem('env-code', 'test');
     }
-    localStorage.setItem('tenant-code', 'default');
-    localStorage.setItem('env-code', 'test');
   };
 
   refresh = () => {
