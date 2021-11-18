3ad75871505047c3a5fe18040b2d5b8bec42ca0d
初旭 <chuxu@shulie.io>
2021-11-18 17:44:22 +0800

    优化

--- src/pages/appManage/components/LinkDbTable.tsx
+++ src/pages/appManage/components/LinkDbTable.tsx
@@ -60,7 +60,7 @@ const LinkDbTable: React.FC<Props> = props => {
                           ? true
                           : false
                       }
-                      titles="新增缓存/连接池"
+                      titles="新增数据库/缓存连接池"
                       action="add"
                       id={id}
                       detailData={detailData}
