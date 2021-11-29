eeba9f99cd005b61346264786a2595ed333dc1fc
谢舒羽 <xieshuyu@xieshuyudeMacBook-Pro.local>
2021-11-29 15:45:25 +0800

    feat: 二期联调

--- src/pages/simulationConfig/modals/ RootDirectory.tsx
+++ src/pages/simulationConfig/modals/ RootDirectory.tsx
@@ -2,9 +2,9 @@ import React, { Fragment, useEffect } from 'react';
 import { CommonModal, CommonTable, useStateReducer } from 'racc';
 import { customColumnProps } from 'src/components/custom-table/utils';
 import { ColumnProps } from 'antd/lib/table';
-import { Form, Input, message } from 'antd';
+import { Form, Input, message, Select } from 'antd';
 import { FormItemProps } from 'antd/lib/form';
-
+const { Option } = Select;
 interface Props {
   form: any;
 }
@@ -46,50 +46,89 @@ const RootDirectory: React.FC<Props> = props => {
           label="类型"
         >
           {props.form.getFieldDecorator('version', {
-            initialValue: '',
+            initialValue: 'oss',
             rules: [{ required: true, message: `请输入类型` }],
           })(
-            <Input placeholder="请输入类型" />
+            <Select placeholder="请选择类型">
+              <Option value="oss">oss</Option>
+              <Option value="ftp">ftp</Option>
+            </Select>
           )}
         </Form.Item>
-        <Form.Item
-          label="地址"
-        >
-          {props.form.getFieldDecorator('version', {
-            initialValue: '',
-            rules: [{ required: true, message: `请输入地址` }],
-          })(
-            <Input placeholder="请输入地址" />
-          )}
-        </Form.Item>
-        <Form.Item
-          label="路径"
-        >
-          {props.form.getFieldDecorator('version', {
-            initialValue: '',
-            rules: [{ required: true, message: `请输入路径` }],
-          })(
-            <Input placeholder="请输入路径" />
-          )}
-        </Form.Item>
-        <Form.Item
-          label="用户名"
-        >
-          {props.form.getFieldDecorator('version', {
-            initialValue: '',
-          })(
-            <Input placeholder="请输入用户名" />
-          )}
-        </Form.Item>
-        <Form.Item
-          label="密码"
-        >
-          {props.form.getFieldDecorator('version', {
-            initialValue: '',
-          })(
-            <Input placeholder="请输入密码" />
-          )}
-        </Form.Item>
+        {
+          props.form.getFieldValue('version') === 'ftp' && (
+            <div>
+              <Form.Item
+                label="ip"
+              >
+                {props.form.getFieldDecorator('ip', {
+                  rules: [{ required: true, message: `请输入地址` }],
+                })(
+                  <Input placeholder="请输入ip" />
+                )}
+              </Form.Item>
+              <Form.Item
+                label="端口"
+              >
+                {props.form.getFieldDecorator('version', {
+                  rules: [{ required: true, message: `请输入端口` }],
+                })(
+                  <Input placeholder="请输入端口" />
+                )}
+              </Form.Item>
+              <Form.Item
+                label="账号"
+              >
+                {props.form.getFieldDecorator('version')(
+                  <Input placeholder="请输入账号" />
+                )}
+              </Form.Item>
+              <Form.Item
+                label="密码"
+              >
+                {props.form.getFieldDecorator('version')(
+                  <Input placeholder="请输入密码" type="password" />
+                )}
+              </Form.Item>
+            </div>
+          )}{ props.form.getFieldValue('version') === 'oss' && (
+            <div>
+              <Form.Item
+                label="endpoint"
+              >
+                {props.form.getFieldDecorator('endpoint', {
+                  rules: [{ required: true, message: `请输入endpoint` }],
+                })(
+                  <Input placeholder="请输入endpoint" />
+                )}
+              </Form.Item>
+              <Form.Item
+                label="accessKeyId"
+              >
+                {props.form.getFieldDecorator('accessKeyId', {
+                  rules: [{ required: true, message: `请输入accessKeyId` }],
+                })(
+                  <Input placeholder="请输入accessKeyId" type="password"/>
+                )}
+              </Form.Item>
+              <Form.Item
+                label="accessKeySecret"
+              >
+                {props.form.getFieldDecorator('accessKeySecret')(
+                  <Input placeholder="请输入accessKeySecret" type="password"/>
+                )}
+              </Form.Item>
+              <Form.Item
+                label="bucketName"
+              >
+                {props.form.getFieldDecorator('bucketName')(
+                  <Input placeholder="请输入bucketName" />
+                )}
+              </Form.Item>
+            </div>
+          )
+        }
+
       </Form>
     </CommonModal>
   );
