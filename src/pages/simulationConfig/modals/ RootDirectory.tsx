34ec082149b66e03134309c8e9f7498d3bcb1416
谢舒羽 <xieshuyu@xieshuyudeMacBook-Pro.local>
2021-11-29 18:18:51 +0800

    feat: 二期联调

--- src/pages/simulationConfig/modals/ RootDirectory.tsx
+++ src/pages/simulationConfig/modals/ RootDirectory.tsx
@@ -2,7 +2,7 @@ import React, { Fragment, useEffect } from 'react';
 import { CommonModal, CommonTable, useStateReducer } from 'racc';
 import { customColumnProps } from 'src/components/custom-table/utils';
 import { ColumnProps } from 'antd/lib/table';
-import { Form, Input, message, Select } from 'antd';
+import { Button, Form, Input, message, Select } from 'antd';
 import { FormItemProps } from 'antd/lib/form';
 import configService from '../service';
 const { Option } = Select;
@@ -12,6 +12,16 @@ interface Props {
 
 const getInitState = () => ({
   data: [],
+  buDisabled: true,
+  datas: {
+    pathType: 0
+  },
+  context: {
+    endpoint: '',
+    bucketName: '',
+    accessKeySecret: '',
+    accessKeyId: ''
+  }
 });
 
 const RootDirectory: React.FC<Props> = props => {
@@ -36,17 +46,48 @@ const RootDirectory: React.FC<Props> = props => {
       data: { data, success }
     } = await configService.pathConfig({});
     if (success) {
-      // console.log(data)
+      if (data.editable === 1) {
+        setState({
+          buDisabled: true,
+        });
+      } else {
+        setState({
+          buDisabled: false,
+        });
+      }
+      setState({
+        datas: data,
+        context: data && JSON.parse(data.context)
+      });
     }
+  };
+
+  const handleCancel = () => {
+    setState({
+      buDisabled: true,
+    });
+    props.form.resetFields();
   };
 
   return (
     <CommonModal
-      beforeOk={handleOk}
       onClick={onClick}
       modalProps={{
-        width: 600,
-        title: '探针根目录编辑'
+        width: 700,
+        title: '探针根目录编辑',
+        footer: [
+          <Button key="back" onClick={handleCancel}>
+            取消
+          </Button>,
+          <Button
+            key="submit"
+            type="primary"
+            onClick={handleOk}
+            disabled={state.buDisabled}
+          >
+            确认编辑
+          </Button>,
+        ]
       }}
       btnProps={{
         type: 'default'
@@ -58,17 +99,17 @@ const RootDirectory: React.FC<Props> = props => {
           label="类型"
         >
           {props.form.getFieldDecorator('version', {
-            initialValue: 'oss',
+            initialValue: `${state.datas?.pathType}`,
             rules: [{ required: true, message: `请输入类型` }],
           })(
             <Select placeholder="请选择类型">
-              <Option value="oss">oss</Option>
-              <Option value="ftp">ftp</Option>
+              <Option value="0">oss</Option>
+              <Option value="1">ftp</Option>
             </Select>
           )}
         </Form.Item>
         {
-          props.form.getFieldValue('version') === 'ftp' && (
+          props.form.getFieldValue('version') === '1' && (
             <div>
               <Form.Item
                 label="ip"
@@ -103,12 +144,13 @@ const RootDirectory: React.FC<Props> = props => {
                 )}
               </Form.Item>
             </div>
-          )}{props.form.getFieldValue('version') === 'oss' && (
+          )}{props.form.getFieldValue('version') === '0' && (
             <div>
               <Form.Item
                 label="endpoint"
               >
                 {props.form.getFieldDecorator('endpoint', {
+                  initialValue: state.context.endpoint,
                   rules: [{ required: true, message: `请输入endpoint` }],
                 })(
                   <Input placeholder="请输入endpoint" />
@@ -118,6 +160,7 @@ const RootDirectory: React.FC<Props> = props => {
                 label="accessKeyId"
               >
                 {props.form.getFieldDecorator('accessKeyId', {
+                  initialValue: state.context.accessKeyId,
                   rules: [{ required: true, message: `请输入accessKeyId` }],
                 })(
                   <Input placeholder="请输入accessKeyId" type="password" />
@@ -126,14 +169,20 @@ const RootDirectory: React.FC<Props> = props => {
               <Form.Item
                 label="accessKeySecret"
               >
-                {props.form.getFieldDecorator('accessKeySecret')(
+                {props.form.getFieldDecorator('accessKeySecret', {
+                  initialValue: state.context.accessKeySecret,
+                  rules: [{ required: true, message: `请输入accessKeySecret` }],
+                })(
                   <Input placeholder="请输入accessKeySecret" type="password" />
                 )}
               </Form.Item>
               <Form.Item
                 label="bucketName"
               >
-                {props.form.getFieldDecorator('bucketName')(
+                {props.form.getFieldDecorator('bucketName', {
+                  initialValue: state.context.bucketName,
+                  rules: [{ required: true, message: `请输入bucketName` }],
+                })(
                   <Input placeholder="请输入bucketName" />
                 )}
               </Form.Item>
