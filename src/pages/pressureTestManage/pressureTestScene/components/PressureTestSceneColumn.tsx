db8a865bf694f5471d07ad2880a169e5e7ca79ca
谢舒羽 <xieshuyu@xieshuyudeMacBook-Pro.local>
2021-12-01 21:00:49 +0800

    去掉时间

--- src/pages/pressureTestManage/pressureTestScene/components/PressureTestSceneColumn.tsx
+++ src/pages/pressureTestManage/pressureTestScene/components/PressureTestSceneColumn.tsx
@@ -231,28 +231,6 @@ const getPressureTestSceneColumns = (state, setState): ColumnProps<any>[] => {
     },
     {
       ...customColumnProps,
-      title: '最新压测时间 / 定时压测时间',
-      dataIndex: 'lastPtTime',
-      render: (text, row) =>
-        text ? (
-          row.isScheduler ? (
-            <Fragment>
-              <div>{text}</div>
-              {row.scheduleExecuteTime && (
-                <div style={{ color: '#bababa' }}>
-                  {row.scheduleExecuteTime}(定时)
-                </div>
-              )}
-            </Fragment>
-          ) : (
-            text
-          )
-        ) : (
-          '--'
-        )
-    },
-    {
-      ...customColumnProps,
       title: '负责人',
       dataIndex: 'userName',
       className: getTakinAuthority() === 'true' ? '' : 'tableHiddle'
