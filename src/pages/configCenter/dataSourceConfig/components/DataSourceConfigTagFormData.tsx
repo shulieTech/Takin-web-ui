b4538ceb75796d473a7cd303105ab386b4e34eab
初旭 <chuxu@shulie.io>
2021-12-21 16:30:38 +0800

    fix bug

--- src/pages/configCenter/dataSourceConfig/components/DataSourceConfigTagFormData.tsx
+++ src/pages/configCenter/dataSourceConfig/components/DataSourceConfigTagFormData.tsx
@@ -4,14 +4,22 @@ import { FormDataType } from 'racc/dist/common-form/type';
 
 const getDataSourceConfigTagFormData = (state, setState): FormDataType[] => {
   const handleChange = (value, option) => {
+    // console.log('value, option', value, option);
     setState({
-      tags: value,
+      tags: value.filter(item => {
+        if (item.trim() !== '') {
+          return item;
+        }
+      }),
       tagsValue: option.map((item, k) => {
+        // if (item.props.children.trim() !== '') {
         return item.props.children;
+        // }
       })
     });
   };
-
+  // console.log('state.tags', state.tags);
+  // console.log('state.tagsValue', state.tagsValue);
   const basicFormData = [
     {
       key: 'tagNames',
@@ -23,7 +31,24 @@ const getDataSourceConfigTagFormData = (state, setState): FormDataType[] => {
             required: false,
             message: '请输入脚本标签'
           }
-        ]
+        ],
+        normalize: value => {
+          const replaceSpace = [];
+
+          value.forEach(item => {
+            const temItem = item.replace(/(^\s+)|(\s+$)/g, '');
+            if (temItem !== '') {
+              // console.log('temItem', temItem);
+              replaceSpace.push(temItem);
+            }
+            // temItem !== '' && replaceSpace.push(temItem);
+          });
+          const filterValue = replaceSpace.filter(
+            item => !/^\s+|\s+$/g.test(item)
+          );
+
+          return filterValue;
+        }
       },
       node: (
         <CommonSelect
