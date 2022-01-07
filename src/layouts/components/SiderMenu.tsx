21808ad99fd503959960d82f2dc0b0ac8fd4fd63
谢舒羽 <xieshuyu@xieshuyudeMacBook-Pro.local>
2022-01-07 11:04:35 +0800

    fix:bug

--- src/layouts/components/SiderMenu.tsx
+++ src/layouts/components/SiderMenu.tsx
@@ -59,28 +59,25 @@ const SiderMenu: React.FC<Props> = props => {
   }, []);
 
   const handleLogout = async () => {
+    const storageList = [
+      'troweb-role',
+      'troweb-userName',
+      'full-link-token',
+      'trowebUserResource',
+      'trowebBtnResource',
+      'auth-cookie',
+      'troweb-expire',
+      'troweb-userId',
+      'trowebUserMenu',
+      'takinAuthority',
+      'Access-Token'
+    ];
+    storageList.forEach(item => localStorage.removeItem(item));
     const {
       data: { success, data }
     } = await UserService.troLogout({
       thirdParty: '1'
     });
-    if (success) {
-      message.success('登录已失效，请重新登录');
-      const storageList = [
-        'troweb-role',
-        'troweb-userName',
-        'full-link-token',
-        'trowebUserResource',
-        'trowebBtnResource',
-        'auth-cookie',
-        'troweb-expire',
-        'troweb-userId',
-        'trowebUserMenu',
-        'takinAuthority',
-        'Access-Token'
-      ];
-      storageList.forEach(item => localStorage.removeItem(item));
-    }
   };
 
   const content = (
