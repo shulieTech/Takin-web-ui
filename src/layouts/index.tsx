05e537fa17c11bd0e8af71fd805c7557e200edb9
李力 <lili@shulie.io>
2022-01-14 15:49:29 +0800

    fix: bug

--- src/layouts/index.tsx
+++ src/layouts/index.tsx
@@ -64,7 +64,7 @@ const IndexLayout: React.FC<Basic.BaseProps> = (props) => {
     <DocumentTitle title={venomBasicConfig.title}>
       <ConfigProvider 
         locale={zh_CN} 
-        getPopupContainer={trigger => window.parent.document.body}
+        getPopupContainer={trigger =>  trigger?.parentElement || window.parent.document.body}
       >
         {layout}
       </ConfigProvider>
