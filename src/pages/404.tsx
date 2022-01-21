9c76d5b62b82c97831187dafe8072fbb0c5eac0c
李力 <lili@shulie.io>
2022-01-21 12:57:21 +0800

    fix: #APFF-615

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -5,7 +5,7 @@ import { withRouter } from 'umi';
 // fix: #APFF-615
 // https://devops.aliyun.com/task/61ea29902ee206003f6f2a2f
 // ⚠︎ iframe的弹窗挂载到父窗口，可能会因为父窗口没有引入样式，导致内容样式丢失，这里干脆全量引入antd样式
-import 'antd/lib/style/index.less';
+import 'antd/lib/style/themes/default.less';
 
 const NotFound = () => {
   return (
