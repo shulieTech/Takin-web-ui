d183f74178001cf345a9417f0b7129dd1c1ed975
李力 <lili@shulie.io>
2022-03-25 17:13:15 +0800

    feat: 趋势详情页面

---
+++ src/pages/pressureTestManage/TrendChart/indexPage.tsx
@@ -0,0 +1,339 @@
+import React, { useState, useEffect } from 'react';
+import { TreeSelect } from 'antd';
+import ReactEcharts from 'echarts-for-react';
+import service from './service';
+
+interface Props {
+  location: {
+    query: any;
+  };
+}
+
+const TrendChart: React.FC<Props> = (props) => {
+  const [query, setQuery] = useState({
+    ...props.location?.query,
+  });
+  const [appList, setAppList] = useState([]);
+  const [chartData, setChartData] = useState({
+    time: [
+      '2022-03-11',
+      '2022-03-12',
+      '2022-03-13',
+      '2022-03-14',
+      '2022-03-15',
+    ],
+    tps: [100, 120, 130, 140, 150],
+    list: [
+      {
+        name: '192.168.1.11',
+        requestFlow: [1, 2, 3, 4, 5],
+        cpu: [20, 40, 30, 24, 15],
+        ram: [200, 400, 100, 400, 250],
+        disk: [30, 20, 25, 58, 100],
+        network: [40, 400, 200, 300, 550],
+      },
+      {
+        name: '192.168.1.33',
+        requestFlow: [4, 2, 1, 6, 35],
+        cpu: [30, 20, 10, 14, 25],
+        ram: [400, 200, 11, 260, 350],
+        disk: [20, 33, 41, 90, 300],
+        network: [51, 41, 141, 231, 45],
+      },
+    ],
+  });
+
+  const commonGrid = {
+    left: '3%',
+    height: 180,
+    width: `43%`,
+    show: true,
+    borderColor: 'none',
+  };
+
+  const seriesConfig = [
+    {
+      name: '请求流量',
+      dataIndex: 'requestFlow',
+      gridCfg: {
+        top: 120,
+      },
+    },
+    {
+      name: 'CPU',
+      dataIndex: 'cpu',
+      gridCfg: {
+        top: 380,
+      },
+    },
+    {
+      name: '内存',
+      dataIndex: 'ram',
+      gridCfg: {
+        top: 380,
+        left: '52%',
+      },
+    },
+    {
+      name: '磁盘',
+      dataIndex: 'disk',
+      gridCfg: {
+        top: 640,
+      },
+    },
+    {
+      name: '网络',
+      dataIndex: 'network',
+      gridCfg: {
+        top: 640,
+        left: '52%',
+      },
+    },
+  ];
+
+  const commonYaxisConfig = {
+    type: 'value',
+    axisLine: {
+      show: false,
+    },
+    axisTick: {
+      show: false,
+    },
+    axisLabel: {
+      color: '#98A1B3',
+      width: 20,
+      overflow: 'truncate',
+      formatter: (val, index) => {
+        return val > 9999 ? `${val / 10000}万` : val;
+      },
+    },
+    splitLine: {
+      show: true,
+      lineStyle: {
+        type: 'dashed',
+        color: ['#E6EAF0', '#fff'],
+      },
+    },
+  };
+
+  const grid = [];
+  const yAxis = [];
+  const xAxis = [];
+  const series = [];
+  const selectedLegend = {};
+
+  seriesConfig.forEach((x, i) => {
+    grid.push({
+      ...commonGrid,
+      ...x.gridCfg,
+    });
+    xAxis.push({
+      type: 'category',
+      boundaryGap: false,
+      axisLine: {
+        onZero: true,
+        lineStyle: {
+          color: '#00BCD4',
+        },
+      },
+      axisTick: {
+        show: false,
+      },
+      axisLabel: {
+        color: '#98A1B3',
+      },
+      data: chartData.time,
+      gridIndex: i,
+    });
+    yAxis.push(
+      {
+        gridIndex: i,
+        name: x.name,
+        nameTextStyle: {
+          align: 'left',
+        },
+        ...commonYaxisConfig,
+      },
+      {
+        name: 'TPS',
+        gridIndex: i,
+        ...commonYaxisConfig,
+        nameTextStyle: {
+          align: 'right',
+        },
+        splitLine: {
+          ...commonYaxisConfig.splitLine,
+          show: false,
+        },
+      }
+    );
+    series.push({
+      type: 'line',
+      showSymbol: true,
+      hoverAnimation: false,
+      smooth: true,
+      // name: 'TPS',
+      xAxisIndex: i,
+      yAxisIndex: 2 * i + 1,
+      data: chartData.tps,
+      color: '#FE7D61',
+      lineStyle: {
+        type: 'dotted',
+      },
+    });
+  });
+
+  (chartData?.list || []).forEach((x) => {
+    // 初始被选中legend的数目
+    if (Object.keys(selectedLegend).length < 5) {
+      selectedLegend[x.name] = true;
+    } else {
+      selectedLegend[x.name] = false;
+    }
+    // 插入所有数据
+    seriesConfig.forEach((y, i) => {
+      series.push({
+        type: 'line',
+        showSymbol: true,
+        hoverAnimation: false,
+        smooth: true,
+
+        name: x.name, // 公用同一个名字的线被划分在一个legend里
+        id: `${x.name}-${y.name}-${i}`,
+        xAxisIndex: i,
+        yAxisIndex: 2 * i,
+        data: x[y.dataIndex] || [],
+        // color: undefined,
+      });
+    });
+  });
+
+  const getAppList = async () => {
+    const {
+      data: { success, data },
+    } = await service.queryLinkChartsInfo({
+      reportId: 2495,
+      xpathMd5: '0f1a197a2040e645dcdb4dfff8a3f960',
+    });
+    if (success) {
+      setAppList(data);
+    }
+  };
+
+  const getChartData = async () => {
+    const {
+      data: { success, data },
+    } = await service.queryLinkChartsInfo({
+      reportId: 2495,
+      xpathMd5: '0f1a197a2040e645dcdb4dfff8a3f960',
+    });
+    if (success) {
+      setChartData(data);
+    }
+  };
+
+  useEffect(() => {
+    // getAppList();
+    // getChartData();
+  }, []);
+
+  return (
+    <div style={{ background: 'var(--Netural-100, #EEF0F2)', padding: 40 }}>
+      <div
+        style={{
+          background: '#fff',
+          padding: 40,
+          borderRadius: 8,
+          boxShadow:
+            '0px 2px 10px rgba(0, 0, 0, 0.05), 0px 1px 4px rgba(0, 0, 0, 0.1)',
+        }}
+      >
+        <TreeSelect
+          showSearch
+          style={{ width: 200 }}
+          searchPlaceholder="搜索应用"
+          value={query?.appId}
+          treeData={appList}
+          onChange={(val) =>
+            setQuery({
+              ...query,
+              appId: val,
+            })
+          }
+          dropdownStyle={{
+            maxHeight: 200,
+          }}
+        />
+
+        <ReactEcharts
+          style={{ width: '100%', height: 1110 }}
+          option={{
+            grid,
+            xAxis,
+            yAxis,
+            series,
+            color: [
+              '#6CBEDC',
+              '#79D193',
+              '#66BCDB',
+              '#ECBB35',
+              '#DF7672',
+              '#5A97E0',
+              '#90CDAC',
+              '#6462B9',
+            ],
+            backgroundColor: '#fff',
+            tooltip: {
+              trigger: 'axis',
+              axisPointer: {
+                animation: false,
+              },
+              formatter: (val) => {
+                let str = `${val[0]?.axisValue} <br>`;
+                let mutiSeriAdded = false;
+                val.forEach((x) => {
+                  const isTps = x.seriesName.startsWith('series');
+                  if (!isTps) {
+                    const typeName = x.seriesId.split('-')?.[1];
+                    str += `${x.marker} ${x.seriesName} ${typeName}: ${x.value}<br>`;
+                  }
+                  if (isTps && !mutiSeriAdded) {
+                    str += `${x.marker} TPS ${x.value}<br>`;
+                    mutiSeriAdded = true;
+                  }
+                  if (isTps) {
+                    str += '<hr style="opacity: 0.4">';
+                  }
+                });
+                return str;
+              },
+            },
+            dataZoom: [
+              {
+                show: true,
+                type: 'slider',
+                top: 10,
+                left: 100,
+                right: 100,
+                realtime: true,
+                start: 0,
+                end: 100,
+                xAxisIndex: [0, 1, 2, 3],
+              },
+            ],
+            axisPointer: {
+              link: { xAxisIndex: 'all' },
+            },
+            legend: {
+              type: 'scroll',
+              top: 50,
+              selected: selectedLegend,
+            },
+          }}
+        />
+      </div>
+    </div>
+  );
+};
+
+export default TrendChart;
