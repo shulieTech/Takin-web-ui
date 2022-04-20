382c58782837d6b2361d0bfb7abda2eae5c0514d
李力 <lili@shulie.io>
2022-04-20 10:33:04 +0800

    fix: 兼容2种flag传参方式

--- src/layouts/SiderLayout.tsx
+++ src/layouts/SiderLayout.tsx
@@ -32,8 +32,11 @@ const SiderLayout: React.FC<SiderLayoutProps> = (props) => {
 
   const pathname: string | any = props.location.pathname;
   const popupDom = useRef(null);
+
+  const thirdPartyLoginFlag = props.location.query.flag || queryString.parse(window.location.search).flag;
+
   useEffect(() => {
-    if (queryString.parse(location.search).flag) {
+    if (thirdPartyLoginFlag) {
       thirdPartylogin();
     } else {
       setState({ request: true });
@@ -44,7 +47,7 @@ const SiderLayout: React.FC<SiderLayoutProps> = (props) => {
     const {
       data: { success, data },
     } = await UserService.thirdPartylogin({
-      flag: queryString.parse(location.search).flag,
+      flag: thirdPartyLoginFlag,
     });
     if (success) {
       if (!data.errorMessage) {
