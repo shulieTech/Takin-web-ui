ef2e5cca9a9d5a8f52d447c6e5df03715c8e8761
李力 <lili@shulie.io>
2022-05-27 13:54:52 +0800

    style: 调试结果列表

--- src/pages/pressureTestManage/testByInterface/components/DebugResult.tsx
+++ src/pages/pressureTestManage/testByInterface/components/DebugResult.tsx
@@ -1,7 +1,10 @@
 import React, { useState, useEffect } from 'react';
-import { Select, Icon, Tree, Pagination } from 'antd';
+import { Select, Icon, Tree, Pagination, Collapse } from 'antd';
 import service from '../service';
 import useListService from 'src/utils/useListService';
+import moment from 'moment';
+
+const { Panel } = Collapse;
 
 interface Props {
   detail: any;
@@ -11,7 +14,6 @@ interface Props {
 const DebugResult: React.FC<Props> = (props) => {
   const { debugId, detail } = props;
   const [result, setResult] = useState();
-
   const { query, total, list, getList, loading } = useListService({
     service: service.getDebugResult,
     defaultQuery: {
@@ -48,6 +50,10 @@ const DebugResult: React.FC<Props> = (props) => {
     }
   }, [debugId]);
 
+  const expandIcon = ({ isActive }) => (
+    <Icon type="caret-right" rotate={isActive ? 90 : 0} />
+  );
+
   return (
     <div>
       <div
@@ -55,22 +61,25 @@ const DebugResult: React.FC<Props> = (props) => {
           padding: '8px 0',
           borderTop: '1px solid #EEF0F2',
           color: 'var(--Netural-500, #AEB2B7)',
+          display: 'flex',
+          alignItems: 'center',
         }}
       >
-        响应结果
-        <div style={{ float: 'right', color: 'var(--Netural-850, #414548)' }}>
-          <span style={{ marginRight: 32 }}>
-            <Icon
-              type="close-circle"
-              theme="filled"
-              style={{ color: '#D64C42', marginRight: 8 }}
-            />
-            1 失败
-          </span>
-          <span style={{ marginRight: 32 }}>
+        <span style={{ flex: 1 }}>响应结果</span>
+        {result && (
+          <div style={{ color: 'var(--Netural-850, #414548)' }}>
+            <span style={{ marginRight: 32 }}>
+              <Icon
+                type="close-circle"
+                theme="filled"
+                style={{ color: '#D64C42', marginRight: 8 }}
+              />
+              1 失败
+            </span>
             <Select
               allowClear
               placeholder="状态"
+              style={{ width: 120 }}
               onChange={(val) =>
                 getList({
                   status: val,
@@ -80,8 +89,8 @@ const DebugResult: React.FC<Props> = (props) => {
               <Select.Option value={1}>成功</Select.Option>
               <Select.Option value={2}>失败</Select.Option>
             </Select>
-          </span>
-        </div>
+          </div>
+        )}
       </div>
       {!result ? (
         <div
@@ -102,9 +111,111 @@ const DebugResult: React.FC<Props> = (props) => {
             borderTop: '1px solid #EEF0F2',
           }}
         >
-          {list.map((x) => {
-            return <div key={x.id}>1111</div>;
-          })}
+          <Collapse
+            bordered={false}
+            expandIcon={expandIcon}
+            style={{
+              backgroundColor: '#fff',
+            }}
+          >
+            {list.map((x) => {
+              const { headers, body, Cookie } = JSON.parse(x.request);
+              const hasError = !!x.errorMessage;
+              return (
+                <Panel
+                  key={x.id}
+                  style={{
+                    borderBottom: 'none',
+                    backgroundColor: hasError ? '#FAF2F3' : '#fff',
+                  }}
+                  header={
+                    <div
+                      style={{
+                        display: 'flex',
+                        color: hasError
+                          ? 'var(--FunctionNegative-500, #D24D40)'
+                          : 'var(--Netural-700, #6F7479)',
+                      }}
+                    >
+                      <div style={{ flex: 1 }}>
+                        <span style={{ marginRight: 16 }}>{x.status}</span>
+                        <span style={{ marginRight: 16 }}>{x.httpMethod}</span>
+                        <span style={{ marginRight: 16 }}>{x.requestUrl}</span>
+                      </div>
+                      <span
+                        style={{
+                          fontSize: 12,
+                          color: 'var(--Netural-500, #AEB2B7)',
+                        }}
+                      >
+                        {moment(x.gmtCreate).format('YYYY-MM-DD HH:mm:ss')}
+                      </span>
+                    </div>
+                  }
+                >
+                  {x.errorMessage && (
+                    <div
+                      style={{
+                        paddingLeft: 16,
+                        color: 'var(--FunctionNegative-500, #D24D40)',
+                        lineHeight: '28px',
+                      }}
+                    >
+                      {x.errorMessage}
+                    </div>
+                  )}
+                  <Collapse
+                    bordered={false}
+                    expandIcon={expandIcon}
+                    style={{
+                      backgroundColor: 'transparent',
+                    }}
+                  >
+                    <Panel
+                      header="Header"
+                      key="Header"
+                      style={{ borderBottom: 'none' }}
+                    >
+                      <div style={{ paddingLeft: 16 }}>
+                        {Object.entries(headers).map(([k, v]) => {
+                          return (
+                            <div key={k} style={{ lineHeight: '28px' }}>
+                              <span
+                                style={{ color: '#6187CF', marginRight: 8 }}
+                              >
+                                {k}：
+                              </span>
+                              <span
+                                style={{
+                                  color: 'var(--Netural-850, #414548)',
+                                }}
+                              >
+                                {v}
+                              </span>
+                            </div>
+                          );
+                        })}
+                      </div>
+                    </Panel>
+                    <Panel
+                      header="Body"
+                      key="Body"
+                      style={{ borderBottom: 'none' }}
+                    >
+                      <div style={{ paddingLeft: 16 }}>{body}</div>
+                    </Panel>
+                    <Panel
+                      header="Response"
+                      key="Response"
+                      style={{ borderBottom: 'none' }}
+                    >
+                      <div style={{ paddingLeft: 16 }}>{x.response}</div>
+                    </Panel>
+                  </Collapse>
+                </Panel>
+              );
+            })}
+          </Collapse>
           <Pagination
             simple
             hideOnSinglePage
