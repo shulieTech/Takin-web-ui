0ae021413dba7efa94c02fb2df103abb49fb6af4
李力 <lili@shulie.io>
2022-05-30 16:19:26 +0800

    revert: 还原文件

---
+++ src/pages/pressureTestManage/testByInterface/components/Introduce.tsx
@@ -0,0 +1,112 @@
+import React from 'react';
+import { Button } from 'antd';
+import bg from 'src/assets/interface-test-bg.png';
+
+interface Props {
+  onCreate: () => void;
+}
+
+const IntroduceSence: React.FC<Props> = (props) => {
+  const infoList = [
+    {
+      title: '场景编辑',
+      desc: '业务流程是压测配置的必填项',
+    },
+    {
+      title: '接口调试',
+      desc: '业务流程是压测配置的必填项',
+    },
+    {
+      title: '压测编辑',
+      desc: '业务流程是压测配置的必填项',
+    },
+    {
+      title: '压测启动',
+      desc: '业务流程是压测配置的必填项',
+    },
+  ];
+  return (
+    <div style={{ margin: 32 }}>
+      <div
+        style={{
+          backgroundImage: `url(${bg}), linear-gradient(90deg, #F3F6F7 0%, #FAFCFC 102.75%)`,
+          backgroundRepeat: 'no-repeat',
+          backgroundSize: 'auto 100%',
+          backgroundPosition: 'right',
+          padding: 32,
+          paddingRight: 340,
+        }}
+      >
+        <h1
+          style={{
+            color: 'var(--Netural-990, #25282A)',
+            fontWeight: 600,
+            fontSize: 20,
+          }}
+        >
+          Takin压测
+        </h1>
+        <div style={{ margin: '16px 0' }}>
+          测试环境单接口压测功能模块简介测试环境单接口压测功能模块简介测试环境单接口压试着创建一个压测场景吧～
+        </div>
+        <Button type="primary" onClick={props.onCreate}>
+          创建压测场景
+        </Button>
+      </div>
+      <div>
+        {infoList.map((x, i, arr) => (
+          <div
+            key={x.title}
+            style={{
+              display: 'flex',
+              alignItems: 'center',
+              padding: '24px 0',
+              borderBottom:
+                i !== arr.length - 1
+                  ? '1px solid var(--Netural-100, #EEF0F2)'
+                  : '',
+            }}
+          >
+            <div
+              style={{
+                width: 72,
+                height: 72,
+                display: 'flex',
+                alignItems: 'center',
+                justifyContent: 'center',
+                border: '1px solid var(--Netural-200, #E5E8EC)',
+                borderRadius: '100%',
+                marginRight: 32,
+                fontSize: 24,
+                color: 'var(--Netural-900, #303336)',
+              }}
+            >
+              {i + 1}
+            </div>
+            <div>
+              <div
+                style={{
+                  fontSize: 16,
+                  color: 'var(--Netural-990, #25282A)',
+                  fontWeight: 500,
+                  marginBottom: 8,
+                }}
+              >
+                {x.title}
+              </div>
+              <div
+                style={{
+                  color: 'var(--Netural-800, #5A5E62)',
+                }}
+              >
+                {x.desc}
+              </div>
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default IntroduceSence;
---
+++ src/pages/pressureTestManage/testByInterface/components/List.tsx
@@ -0,0 +1,192 @@
+import React, { useState, useEffect, useContext } from 'react';
+import {
+  Dropdown,
+  Input,
+  Icon,
+  Spin,
+  Menu,
+  Modal,
+  message,
+  Pagination,
+} from 'antd';
+import useListService from 'src/utils/useListService';
+import service from '../service';
+import styles from '../index.less';
+import classNames from 'classnames';
+import { router } from 'umi';
+import { SenceContext } from '../indexPage';
+
+interface Props {
+  currentSence: any;
+  setCurrentSence: (sence: any) => void;
+}
+
+const SenceList: React.FC<Props> = (props) => {
+  const { currentSence, setCurrentSence } = props;
+  const { hasUnsaved, setHasUnsaved } = useContext(SenceContext);
+  const { list, loading, total, query, getList } = useListService({
+    service: service.getSenceList,
+    defaultQuery: {
+      current: 0,
+      pageSize: 10,
+    },
+    // isQueryOnQueryChange: false,
+  });
+
+  const checkSave = (callback) => {
+    if (hasUnsaved) {
+      Modal.confirm({
+        title: '提示',
+        content: '您的场景有内容修改，切换场景将清空修改内容，是否保存场景？',
+        okText: '保存场景',
+        onOk: async () => {
+          // TODO 保存场景
+        },
+        onCancel: () => {
+          callback();
+          setHasUnsaved(false);
+        },
+      });
+    } else {
+      callback();
+    }
+  };
+
+  const toLive = (sence, event) => {
+    event.stopPropagation();
+    checkSave(() => {
+      router.push(
+        `/pressureTestManage/pressureTestReport/pressureTestLive?id=${sence.id}`
+      );
+    });
+  };
+
+  const deleteSence = async (sence, event) => {
+    event.stopPropagation();
+    Modal.confirm({
+      title: '提示',
+      content: '确认删除？',
+      onOk: async () => {
+        const {
+          data: { success },
+        } = await service.deleteSence(sence.id);
+        if (success) {
+          message.success('操作成功');
+          getList({ current: 0 });
+          if (sence.id === currentSence.id) {
+            setCurrentSence(null);
+          }
+        }
+      },
+    });
+  };
+
+  return (
+    <div>
+      <div style={{ display: 'flex', alignItems: 'center', padding: 8 }}>
+        <Input.Search
+          style={{ flex: 1 }}
+          placeholder="请输入"
+          onSearch={(val) =>
+            getList({
+              keyword: val?.trim(),
+            })
+          }
+        />
+        <Icon
+          type="plus"
+          className="pointer"
+          style={{ marginLeft: 16, marginRight: 8 }}
+          onClick={() => setCurrentSence({})}
+        />
+      </div>
+      <div>
+        <Spin spinning={loading}>
+          {total === 0 && !query.keyword && (
+            <div
+              style={{
+                textAlign: 'center',
+                padding: '70px 40px',
+                lineHeight: '24px',
+                color: 'var(--Netural-800, ##5A5E62)',
+              }}
+            >
+              当前场景列表还是空的哦，试着创建一个单接口压测场景吧
+            </div>
+          )}
+          {list.map((x) => {
+            return (
+              <div
+                key={x.id}
+                className={classNames(styles['sence-item'], {
+                  [styles.active]: x.id === currentSence?.id,
+                })}
+                onClick={() => {
+                  checkSave(() => {
+                    setCurrentSence(x);
+                  });
+                }}
+              >
+                <div style={{ display: 'flex' }}>
+                  <span
+                    style={{
+                      color: 'var(--Netural-850, #414548)',
+                      fontWeight: 500,
+                      marginRight: 12,
+                    }}
+                  >
+                    GET
+                  </span>
+                  <span style={{ flex: 1 }} className="truncate" title={x.name}>
+                    {x.name}
+                  </span>
+                  <Dropdown
+                    overlay={
+                      <Menu style={{ width: 140 }}>
+                        {x.status === 2 && (
+                          <Menu.Item
+                            onClick={({ domEvent }) => toLive(x, domEvent)}
+                          >
+                            查看实况
+                          </Menu.Item>
+                        )}
+                        <Menu.Item
+                          style={{ color: 'red' }}
+                          onClick={({ domEvent }) => deleteSence(x, domEvent)}
+                        >
+                          删除
+                        </Menu.Item>
+                      </Menu>}
+                  >
+                    <Icon type="more" className={styles['icon-more']} />
+                  </Dropdown>
+                </div>
+                <div
+                  className="truncate"
+                  style={{
+                    color: 'var(--Netural-600, #90959A)',
+                  }}
+                >
+                  {x.url}
+                </div>
+              </div>
+            );
+          })}
+          <Pagination
+            simple
+            hideOnSinglePage
+            size="small"
+            total={total}
+            pageSize={query.pageSize}
+            current={query.current + 1}
+            onChange={(page, pageSize) => {
+              getList({ pageSize, current: page - 1 });
+            }}
+          />
+        </Spin>
+      </div>
+    </div>
+  );
+};
+
+export default SenceList;
