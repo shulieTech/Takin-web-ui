214e4c1e3511e27f29114e038e3c1d38a079b013
李力 <lili@shulie.io>
2022-07-13 13:37:56 +0800

    chore: build时添加git信息

---
+++ bin/version.js
@@ -0,0 +1,15 @@
+const { exec } = require('child_process');
+const fs = require('fs');
+
+const filePath = './dist/version.log';
+
+fs.writeFileSync(filePath, '');
+
+exec(
+  `git branch --show-current > ${filePath} && git log -n 1 >> ${filePath}`,
+  (err, stdout, stderr) => {
+    if (!err) {
+      console.log(`git版本信息已写入到：${filePath}`);
+    }
+  }
+);
