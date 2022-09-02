ad7e9b143c9a0f034da5a69f2b23c33a41b3d4f9
李力 <lili@shulie.io>
2022-09-02 13:53:09 +0800

    fix: 介绍页调整

--- src/pages/prepareShadowSource/components/Introduce.tsx
+++ src/pages/prepareShadowSource/components/Introduce.tsx
@@ -1,6 +1,6 @@
 /* tslint:disable */
 import React, { useState, useContext } from 'react';
-import { Button, Tooltip, Row, Col } from 'antd';
+import { Button, Tooltip, Row, Col, Icon } from 'antd';
 import styles from '../index.less';
 import classNames from 'classnames';
 import { PrepareContext } from '../indexPage';
@@ -21,31 +21,51 @@ export default (props: Props) => {
         {
           title: '链路梳理',
           infos: [
-            <>
-              梳理链路
-              <br />
-              先梳理压测链路，将链路的入口url梳理出来
-            </>,
-            <>
-              接应用
-              <br />
-              开始接入链路涉及的应用
-            </>,
+            {
+              icon: 'icon-a-ziyuanzhunbeishuoming01-01shulilianlu',
+              text: (
+                <>
+                  梳理链路
+                  <br />
+                  与团队沟通，获得需压测链路的入口url，以及压测范围内的应用信息和节点总数
+                </>
+              ),
+            },
+            {
+              icon: 'icon-a-ziyuanzhunbeishuoming01-02jieruyingyong',
+              text: (
+                <>
+                  接入应用
+                  <br />
+                  推进运维，使用正确的安装方式接入需要的应用
+                </>
+              ),
+            },
           ],
         },
         {
           title: '创建链路',
           infos: [
-            <>
-              创建链路
-              <br />
-              手动创建链路(串联业务环节)、jmeter脚本扫描2种方式创建
-            </>,
-            <>
-              维护链路
-              <br />
-              修改链路名称、入口url对应的业务环节名称
-            </>,
+            {
+              icon: 'icon-a-ziyuanzhunbeishuoming02-01chuangjianlianlu',
+              text: (
+                <>
+                  创建链路
+                  <br />
+                  手动创建链路(串联业务环节)、jmeter脚本扫描2种方式创建
+                </>
+              ),
+            },
+            {
+              icon: 'icon-a-ziyuanzhunbeishuoming02-02weihulianlu',
+              text: (
+                <>
+                  维护链路
+                  <br />
+                  修改链路名称、入口url对应的业务环节名称
+                </>
+              ),
+            },
           ],
         },
       ],
@@ -56,31 +76,41 @@ export default (props: Props) => {
         {
           title: '应用接入',
           infos: [
-            <>
-              接入应用
-              <br />
-              检查链路上的应用是否都已接入
-            </>,
-            <>
-              节点信息
-              <br />
-              检查节点数是否为预期要接入数量，检查已接入探针数是否符合预期
-            </>,
-            <>
-              应用状态
-              <br />
-              检查应用状态是否正常
-            </>,
+            {
+              icon: 'icon-a-ziyuanzhunbeishuoming03-01heduijiedianxinxi',
+              text: (
+                <>
+                  核对节点信息
+                  <br />
+                  检查各应用节点数是否为预期要接入数量，检查已接入探针数是否符合预期
+                </>
+              ),
+            },
+            {
+              icon: 'icon-a-ziyuanzhunbeishuoming03-02jianchayingyongzhuangtai',
+              text: (
+                <>
+                  检查应用状态
+                  <br />
+                  检查应用状态是否正常，可通过应用详情查看具体异常
+                </>
+              ),
+            },
           ],
         },
         {
           title: '压测范围设置',
           infos: [
-            <>
-              查看应用范围
-              <br />
-              检查链路上的应用是否需要加入压测范围
-            </>,
+            {
+              icon: 'icon-a-ziyuanzhunbeishuoming04-01chakanyingyongfanwei',
+              text: (
+                <>
+                  查看应用范围
+                  <br />
+                  检查链路上的应用是否需要加入压测范围
+                </>
+              ),
+            },
           ],
         },
       ],
@@ -89,33 +119,64 @@ export default (props: Props) => {
       title: '隔离配置',
       cards: [
         {
-          title: '选择隔离方案',
+          title: '选择隔离方式',
           infos: [
-            <>
-              隔离方案
-              <br />
-              影子库、影子表、影子库/表3种方案
-            </>,
+            {
+              icon: 'icon-a-ziyuanzhunbeishuoming05-01goutongquerengelifangan',
+              text: (
+                <>
+                  沟通确认隔离方案
+                  <br />
+                  与项目成员沟通一致的隔离配置方案，建议项目范围内使用一种隔离方案
+                </>
+              ),
+            },
+            {
+              icon: 'icon-a-ziyuanzhunbeishuoming05-02zaiTakinzhongshezhilianlugelifangan',
+              text: (
+                <>
+                  在Takin中设置链路隔离方案
+                  <br />
+                  初始化链路需要进行链路隔离方案设置，设置后
+                  Takin自动识别数据源信息和链接状态
+                </>
+              ),
+            },
           ],
         },
         {
           title: '配置隔离方案',
           infos: [
-            <>
-              检查数据源
-              <br />
-              检查自动识别的数据源是否齐全，可手工新增
-            </>,
-            <>
-              协同推进
-              <br />
-              导出配置方案，并联系DBA协助完成隔离配置
-            </>,
-            <>
-              配置
-              <br />
-              DBA按照需求进行隔离配置
-            </>,
+            {
+              icon: 'icon-a-ziyuanzhunbeishuoming06-01jianchashujuyuan',
+              text: (
+                <>
+                  检查数据源
+                  <br />
+                  检查自动识别的数据源是否齐全，可手工新增
+                </>
+              ),
+            },
+            {
+              icon: 'icon-a-ziyuanzhunbeishuoming06-02xietongtuijin',
+              text: (
+                <>
+                  协同推进
+                  <br />
+                  导出配置方案，并联系DBA协助完成隔离配置
+                </>
+              ),
+            },
+            {
+              icon: 'icon-a-ziyuanzhunbeishuoming06-03peizhiyingzishujuyuan',
+              text: (
+                <>
+                  配置
+                  <br />
+                  DBA按照需求进行隔离配置
+                </>
+              ),
+            },
           ],
         },
       ],
@@ -126,21 +187,52 @@ export default (props: Props) => {
         {
           title: '配置准备检查',
           infos: [
-            <>
-              应用检查
-              <br />
-              接入应用与预期接入数一致，加入压测范围的应用状态都正常
-            </>,
-            <>
-              影子资源检查
-              <br />
-              数据源配置信息完整，检查影子资源连通、配置生效
-            </>,
+            {
+              icon: 'icon-a-ziyuanzhunbeishuoming07-01yingyongjiancha',
+              text: (
+                <>
+                  应用检查
+                  <br />
+                  接入应用与预期接入数一致，加入压测范围的应用状态都正常
+                </>
+              ),
+            },
+            {
+              icon: 'icon-a-ziyuanzhunbeishuoming07-02yingziziyuanjiancha',
+              text: (
+                <>
+                  影子资源检查
+                  <br />
+                  数据源配置信息完整，检查影子资源连通、配置生效
+                </>
+              ),
+            },
           ],
         },
         {
           title: '开始下一环节',
-          infos: [<>链路全部检查完成后，便可进入链路调试或开始压测</>],
+          infos: [
+            {
+              icon: 'icon-a-ziyuanzhunbeishuoming08-01lianlutiaoshi',
+              text: (
+                <>
+                  链路调试
+                  <br />
+                  链路压测资源准备完成后可以线进行调试，可以发现一些
+                </>
+              ),
+            },
+            {
+              icon: 'icon-a-ziyuanzhunbeishuoming01-02jieruyingyong',
+              text: (
+                <>
+                  压测
+                  <br />
+                  调试完成后，在进行简单的压测配置，就可以进行压测啦
+                </>
+              ),
+            },
+          ],
         },
       ],
     },
@@ -155,7 +247,7 @@ export default (props: Props) => {
             fontWeight: 600,
           }}
         >
-          链路资源准备
+          链路资源准备使用说明
         </div>
         <div
           style={{
@@ -186,22 +278,41 @@ export default (props: Props) => {
       </div>
 
       <div>
-        <div style={{ marginBottom: 40 }}>
-          {list.map((x, i) => {
+        <div
+          style={{ marginBottom: 40, display: 'flex', alignItems: 'center' }}
+        >
+          {list.map((x, i, arr) => {
             return (
-              <Button
-                key={x.title}
-                shape="round"
-                className={classNames(styles['round-btn'], {
-                  [styles.active]: activeKey === i,
-                })}
-                style={{
-                  marginRight: 64,
-                }}
-                onClick={() => setActiveKey(i)}
-              >
-                {x.title}
-              </Button>
+              <>
+                <Button
+                  key={x.title}
+                  shape="round"
+                  className={classNames(styles['round-btn'], {
+                    [styles.active]: activeKey === i,
+                  })}
+                  style={{
+                    color:
+                      activeKey === i
+                        ? 'var(--Netural-900, #303336)'
+                        : 'inherit',
+                  }}
+                  onClick={() => setActiveKey(i)}
+                >
+                  <span
+                    style={{
+                      color: 'var(--Brandprimary-500, #0FBBD5)',
+                      marginRight: 16,
+                      fontWeight: 500,
+                    }}
+                  >
+                    Step {i + 1}
+                  </span>
+                  <span>{x.title}</span>
+                </Button>
+                {i < arr.length - 1 && (
+                  <Icon type="right" style={{ margin: '0 24px' }} />
+                )}
+              </>
             );
           })}
         </div>
@@ -210,45 +321,50 @@ export default (props: Props) => {
           {list.map((x, i) => {
             return (
               activeKey === i && (
-                <Row gutter={40} key={i}>
+                <div
+                  key={i}
+                  style={{ display: 'flex', justifyContent: 'space-between' }}
+                >
                   {x.cards.map((y, j) => {
                     return (
-                      <Col span={12} key={`${i}-${j}`}>
-                        <div className={styles.card}>
-                          <span className={styles.order}>{j + 1}</span>
-                          <div className={styles.title}>{y.title}</div>
-                          <div style={{ marginTop: 40 }}>
-                            {y.infos.map((z, m) => {
-                              return (
+                      <div
+                        className={styles.card}
+                        style={{ width: '48%' }}
+                        key={`${i}-${j}`}
+                      >
+                        <div className={styles.title}>{y.title}</div>
+                        <div style={{ marginTop: 40 }}>
+                          {y.infos.map((z, m) => {
+                            return (
+                              <div
+                                key={`${i}-${j}-${m}`}
+                                style={{ display: 'flex' }}
+                              >
+                                <span
+                                  className={classNames(
+                                    styles.icon,
+                                    'iconfont',
+                                    z.icon
+                                  )}
+                                />
                                 <div
-                                  key={`${i}-${j}-${m}`}
-                                  style={{ display: 'flex' }}
+                                  style={{
+                                    flex: 1,
+                                    padding: 16,
+                                    borderTop: '1px solid #F7F8FA',
+                                    lineHeight: '28px',
+                                  }}
                                 >
-                                  <span
-                                    className={classNames(
-                                      styles.icon,
-                                      'iconfont '
-                                    )}
-                                  />
-                                  <div
-                                    style={{
-                                      flex: 1,
-                                      padding: 16,
-                                      borderTop: '1px solid #F7F8FA',
-                                      lineHeight: '28px',
-                                    }}
-                                  >
-                                    {z}
-                                  </div>
+                                  {z.text}
                                 </div>
-                              );
-                            })}
-                          </div>
+                              </div>
+                            );
+                          })}
                         </div>
-                      </Col>
+                      </div>
                     );
                   })}
-                </Row>
+                </div>
               )
             );
           })}
