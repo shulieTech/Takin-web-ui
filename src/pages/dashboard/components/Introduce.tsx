7508ab2c0c0c7891117928449bc141df091e0594
李力 <lili@shulie.io>
2022-09-14 09:59:16 +0800

    fix: 文案调整

--- src/pages/dashboard/components/Introduce.tsx
+++ src/pages/dashboard/components/Introduce.tsx
@@ -37,8 +37,6 @@ export default (props) => {
               paddingRight: 24,
             }}
           >
-            无需Jmeter
-            <span className={styles.divider} />
             无需手动梳理隔离数据
             <span className={styles.divider} />
             数据变更后也能精准识别
