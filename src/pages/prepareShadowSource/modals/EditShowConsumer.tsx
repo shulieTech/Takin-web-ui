ca3db6837647369bed9804f68106ec56df1d0dc7
李力 <lili@shulie.io>
2022-10-12 11:33:02 +0800

    fix: 编辑影子消费者逻辑调整

--- src/pages/prepareShadowSource/modals/EditShowConsumer.tsx
+++ src/pages/prepareShadowSource/modals/EditShowConsumer.tsx
@@ -1,7 +1,12 @@
 import React, { useState, useContext, useMemo } from 'react';
 import { message, Modal } from 'antd';
-import { Form, FormItem, createAsyncFormActions } from '@formily/antd';
-import { Input, Select, Radio } from '@formily/antd-components';
+import {
+  Form,
+  FormItem,
+  createAsyncFormActions,
+  FormEffectHooks,
+} from '@formily/antd';
+import { Input, Select, Radio, NumberPicker } from '@formily/antd-components';
 import service from '../service';
 import { PrepareContext } from '../_layout';
 
@@ -37,6 +42,27 @@ export default (props: Props) => {
     }
   };
 
+  const formEffects = () => {
+    const { onFieldValueChange$, onFieldInputChange$ } = FormEffectHooks;
+    onFieldValueChange$('type').subscribe(({ value }) => {
+      const isKafka = value === 'KAFKA';
+      actions.setFieldState(
+        'shadowconsumerEnable',
+        (state) => (state.visible = !isKafka)
+      );
+      actions.setFieldState(
+        '*(isCluster,kafkaType)',
+        (state) => (state.visible = isKafka)
+      );
+    });
+    onFieldValueChange$('isCluster').subscribe(({ value }) => {
+      actions.setFieldState(
+        '*(clusterName,monitorurl,poolSize)',
+        (state) => (state.visible = value === '1')
+      );
+    });
+  };
+
   return (
     <Modal
       title={`${detail?.id ? '编辑' : '新增'}影子消费者`}
@@ -56,8 +82,9 @@ export default (props: Props) => {
       <Form
         actions={actions}
         initialValues={detail}
-        labelCol={6}
-        wrapperCol={18}
+        labelCol={8}
+        wrapperCol={16}
+        effects={formEffects}
       >
         <FormItem
           name="type"
@@ -68,6 +95,21 @@ export default (props: Props) => {
             placeholder: '请选择',
           }}
           rules={[{ required: true, message: '请选择MQ类型' }]}
+        />
+        <FormItem
+          name="kafkaType"
+          title="生产/消费"
+          visible={false}
+          component={Select}
+          dataSource={[
+            { label: '生产', value: 1 },
+            { label: '消费', value: 2 },
+          ]}
+          props={{
+            placeholder: '请选择',
+          }}
+          rules={[{ required: true, message: '请选择生产/消费' }]}
+          initialValue={1}
         />
         <FormItem
           name="topicGroup"
@@ -81,14 +123,21 @@ export default (props: Props) => {
             },
             {
               validator: (val) => {
-                return !val || (typeof val === 'string' && val.length > 0 && val.indexOf('#') > -1)
+                return !val ||
+                  (typeof val === 'string' &&
+                    val.length > 0 &&
+                    val.indexOf('#') > -1)
                   ? ''
                   : '业务的topic#业务的消费组请以#分割';
               },
             },
           ]}
-          props={{ maxLength: 200, whitespace: true, placeholder: '请输入' }}
+          props={{
+            maxLength: 25,
+            placeholder: '请输入业务的topic#业务的消费组',
+          }}
         />
+
         <FormItem
           name="shadowconsumerEnable"
           title="是否消费"
@@ -99,6 +148,67 @@ export default (props: Props) => {
             { label: '消费影子topic', value: '1' },
           ]}
           initialValue={'0'}
+        />
+        <FormItem
+          name="isCluster"
+          title="是否影子集群"
+          visible={false}
+          component={Radio.Group}
+          rules={[{ required: true, message: '请选择是否影子集群' }]}
+          dataSource={[
+            { label: '否', value: '0' },
+            { label: '是', value: '1' },
+          ]}
+          initialValue={'0'}
+        />
+        <FormItem
+          name="clusterName"
+          title="影子集群名称"
+          component={Input}
+          rules={[
+            {
+              required: true,
+              whitespace: true,
+              message: '请输入影子集群名称',
+            },
+          ]}
+          props={{
+            maxLength: 200,
+            placeholder: '请输入影子集群名称clusterName（多个用逗号隔开）',
+          }}
+        />
+        <FormItem
+          name="monitorurl"
+          title="影子集群地址"
+          component={Input}
+          rules={[
+            {
+              required: true,
+              whitespace: true,
+              message: '请输入影子集群地址',
+            },
+          ]}
+          props={{
+            maxLength: 200,
+            placeholder: '请输入影子集群地址monitorurl（多个用逗号隔开）',
+          }}
+        />
+        <FormItem
+          name="poolSize"
+          title="影子生产使用消费线程数"
+          component={NumberPicker}
+          rules={[
+            {
+              required: true,
+              format: 'integer',
+              message: '请输入影子生产使用消费线程数',
+            },
+          ]}
+          props={{
+            placeholder: '请输入影子生产使用消费线程数poolSize',
+            style: { width: '100%' },
+            min: 0,
+          }}
         />
       </Form>
     </Modal>
