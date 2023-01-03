d5b7b165881948cd901b4822c12a02c29fe25f85
{chuxu@shulie.io} <chuxu@shulie.io>
2023-01-03 11:36:16 +0800

    恢复

--- src/pages/pressureTestManage/pressureTestReport/indexPage.tsx
+++ src/pages/pressureTestManage/pressureTestReport/indexPage.tsx
@@ -1,4 +1,3 @@
-/* eslint-disable react-hooks/exhaustive-deps */
 import React, { useEffect, useState, Fragment } from 'react';
 import SearchTable from 'src/components/search-table';
 
@@ -6,8 +5,7 @@ import { useStateReducer } from 'racc';
 
 import getPressureTestReportColumns from './components/PressureTestReportColumn';
 import getPressureTestReportFormData from './components/PressureTestReportFormData';
-import { Button } from 'antd';
-import router from 'umi/router';
+
 interface PressureTestReportProps {
   location?: { query?: any };
 }
@@ -18,77 +16,27 @@ export interface PressureTestReportState {
     current: string | number;
     pageSize: string | number;
   };
-  batchSelectedKeys: number[];
-  sameScenceId: string | number; // 只能对比选择同一个场景的报告
 }
 
-const PressureTestReport: React.FC<PressureTestReportProps> = (props) => {
+const PressureTestReport: React.FC<PressureTestReportProps> = props => {
   const [state, setState] = useStateReducer<PressureTestReportState>({
     isReload: false,
     searchParams: {
+      current: 0,
+      pageSize: 10,
       ...props.location.query,
-      current: Number(props.location.query?.current) ||  0,
-      pageSize: Number(props.location.query?.pageSize) || 10,
     },
-    batchSelectedKeys: [],
-    sameScenceId: undefined,
   });
 
   return (
     <Fragment>
       <SearchTable
-        tableAction={
-          <Button
-            type="primary"
-            disabled={!(state.batchSelectedKeys?.length > 1)}
-            onClick={() => {
-              router.push({
-                pathname: '/pressureTestManage/pressureTestReport/compare',
-                query: {
-                  sceneId: state.sameScenceId,
-                  reportIds: state.batchSelectedKeys,
-                }
-              });
-            }}
-          >
-            报告对比
-          </Button>
-        }
         commonTableProps={{
-          rowKey: 'id',
-          columns: getPressureTestReportColumns(state, setState),
-          rowSelection: {
-            type: 'checkbox',
-            // hideDefaultSelections: true,
-            columnTitle: ' ',
-            selectedRowKeys: state.batchSelectedKeys,
-            getCheckboxProps: (record) => {
-              const isDiffrentScene = state.sameScenceId ? record.sceneId !== state.sameScenceId : false;
-              const isSelected = state.batchSelectedKeys.includes(record.id);
-              return {
-                disabled: isDiffrentScene || (!isSelected && state.batchSelectedKeys.length > 4),
-              };
-            },
-            onChange: (selectedKeys, selectedRows) => {
-              if (!state.sameScenceId) {
-                setState({
-                  sameScenceId: selectedRows[0].sceneId,
-                });
-              }
-              if (selectedKeys.length === 0) {
-                setState({
-                  sameScenceId: undefined,
-                });
-              }
-              setState({
-                batchSelectedKeys: selectedKeys.slice(0, 5),
-              });
-            },
-          },
+          columns: getPressureTestReportColumns(state, setState)
         }}
         commonFormProps={{
           formData: getPressureTestReportFormData(),
-          rowNum: 4,
+          rowNum: 4
         }}
         ajaxProps={{ url: '/report/listReport', method: 'GET' }}
         searchParams={state.searchParams}
@@ -96,8 +44,8 @@ const PressureTestReport: React.FC<PressureTestReportProps> = (props) => {
         datekeys={[
           {
             originKey: 'time',
-            separateKey: ['startTime', 'endTime'],
-          },
+            separateKey: ['startTime', 'endTime']
+          }
         ]}
       />
     </Fragment>
