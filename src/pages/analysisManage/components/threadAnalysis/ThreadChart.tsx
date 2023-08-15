0bbb9da82b73d718fb3d6df9f955798fc3c155d5
{chuxu@shulie.io} <chuxu@shulie.io>
2023-08-15 10:45:49 +0800

    fix 性能分析

--- src/pages/analysisManage/components/threadAnalysis/ThreadChart.tsx
+++ src/pages/analysisManage/components/threadAnalysis/ThreadChart.tsx
@@ -102,7 +102,7 @@ const ThreadChart: React.FC<Props> = ({
             const keys = Object.keys(data);
             return {
               name: labelMap[keys[1]],
-              value: data.threadCount && data.threadCount !== 0 || `${data.cpuRate}%`
+              value: `${data.threadCount}`  || `${data.cpuRate}%`
             };
           },
           crosshairs: {
